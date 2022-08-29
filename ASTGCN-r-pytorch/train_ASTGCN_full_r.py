7ceb40362416b301d41b55874dcfc81396cd46fb
Nguyễn Phú Trường (VNCDLL-CTÐTKSAI-HVCTÐTKSAI) <truongnp3@vingroup.net>
2022-08-29 09:23:41 +0700

    add ASTGCN_Full config, fix using cpu instead of GPU

--- ASTGCN-r-pytorch/train_ASTGCN_full_r.py
+++ ASTGCN-r-pytorch/train_ASTGCN_full_r.py
@@ -43,8 +43,8 @@ model_name = training_config['model_name']
 ctx = training_config['ctx']
 os.environ["CUDA_VISIBLE_DEVICES"] = ctx
 USE_CUDA = torch.cuda.is_available()
-DEVICE = torch.device('cpu')
-# print("CUDA:", USE_CUDA, DEVICE)
+DEVICE = torch.device('cuda:0')
+print("CUDA:", USE_CUDA, DEVICE)
 
 learning_rate = float(training_config['learning_rate'])
 epochs = int(training_config['epochs'])
